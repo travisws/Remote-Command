3cb8e36e2e8f40abf26f3e8bc2846f78361360b8
Travisws <traviswoodworthsmith@gmail.com>
2018-03-05 22:24:29 -0700

    Got started on sockets and add as a command witch I need to rename.

---
+++ src/com/theraven/CommandKit.java
@@ -0,0 +1,24 @@
+package com.theraven;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandKit implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            // Here we need to give items to our player
+
+            testSocket testSocket = new testSocket();
+            testSocket.start();
+            System.out.println("testSocket word");
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+}
